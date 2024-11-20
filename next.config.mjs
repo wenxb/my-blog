d679d613bbdc1687d773dc07caaab9a207d9e5d5
youngwen <i@youngwen.com>
2024-11-20 22:52:02 +0800

    p

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,70 @@
 import {withContentlayer} from 'next-contentlayer'
 import {initField, startFileWatcher} from "./scripts/post.mjs";
-import {join} from "node:path";
-import {access, symlink} from "node:fs";
+import path from "node:path";
+import fs from "node:fs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     webpack: (config, {isServer, dev}) => {
-        config.experiments = {
+        config.experiments = Object.assign(config.experiments || {}, {
             layers: true,
             asyncWebAssembly: true,
-            syncWebAssembly: true,
-        };
-        config.optimization.moduleIds = 'named';
-
+        });
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: 'webassembly/async',
+        });
         if (!isServer) {
             config.output.environment = { ...config.output.environment, asyncFunction: true };
         }
-        config.output.webassemblyModuleFilename = isServer ? '../static/wasm/[modulehash].wasm' : 'static/wasm/[modulehash].wasm';
+        config.output.webassemblyModuleFilename = isServer && !dev ? './../static/wasm/[modulehash].wasm' : 'static/wasm/[modulehash].wasm';
+
+        config.plugins.push(
+            new (class {
+                apply(compiler) {
+                    compiler.hooks.afterEmit.tapPromise(
+                        'WasmFileWebpackPlugin',
+                        async (compiler) => {
+                            if(isServer){
+                                const wasmFolder =  path.join(compiler.options.output.path, 'static/wasm')
+                                const targetFolder =  path.join(compiler.options.output.path, '../static/wasm')
+
+                                try {
+                                    if(fs.existsSync(wasmFolder)){
+                                        const files = fs.readdirSync(wasmFolder);
+                                        if (!fs.existsSync(targetFolder)) {
+                                            fs.mkdirSync(targetFolder, { recursive: true });
+                                        }
+
+                                        files.forEach((file) => {
+                                            const sourcePath = path.join(wasmFolder, file);
+                                            const targetPath = path.join(targetFolder, file);
+                                            if (fs.existsSync(targetPath)) {
+                                                console.log(`文件已存在，跳过: ${file}`);
+                                                return;
+                                            }
+
+                                            // 复制文件
+                                            try {
+                                                fs.copyFileSync(sourcePath, targetPath);
+                                                console.log(`文件Copy成功: ${file}`);
+                                            } catch (err) {
+                                                console.error(`Copy文件失败: ${file}`, err);
+                                            }
+                                        });
+                                    }
+                                } catch (err) {
+                                    console.error('读取Wasm文件夹失败:', err);
+                                }
+                            }
+                        },
+                    );
+                }
+            })(),
+        );
+
 
         const postPath = "./content/post"
         if (dev) {
@@ -27,34 +73,6 @@ const nextConfig = {
             initField(postPath)
         }
 
-        config.plugins.push(
-            new (class {
-                apply(compiler) {
-                    compiler.hooks.afterEmit.tapPromise(
-                        'SymlinkWebpackPlugin',
-                        async (compiler) => {
-                            if (isServer) {
-                                const from = join(compiler.options.output.path, '../static');
-                                const to = join(compiler.options.output.path, 'static');
-
-                                try {
-                                    await access(from,()=>{});
-                                } catch (error) {
-                                    if (error.code === 'ENOENT') {
-                                        // No link exists
-                                    } else {
-                                        throw error;
-                                    }
-                                }
-
-                                await symlink(to, from, 'junction',()=>{});
-                                console.log(`created symlink ${from} -> ${to}`);
-                            }
-                        },
-                    );
-                }
-            })(),
-        );
         return config;
     }
 }
