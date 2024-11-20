1fa068da9836583b48c1060e86a947551f938e3e
youngwen <i@youngwen.com>
2024-11-20 19:35:12 +0800

    p

--- components/page/music/SideLyric.js
+++ components/page/music/SideLyric.js
@@ -1,9 +1,10 @@
 "use client";
 import {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
-import {LyricPlayer} from "@applemusic-like-lyrics/react"
 import {cloneDeep} from "lodash"
 import "./SideLyric.scss"
+import NoSsr from "@/components/common/NoSsr";
+import {LyricPlayer} from "@applemusic-like-lyrics/react";
 
 const SideLyric = () => {
     const playSongId = useSelector(state => state.musicData.playSongData?.id)
@@ -11,6 +12,7 @@ const SideLyric = () => {
     const [lyricLines, setLyricLines] = useState([]);
     const isPlaying = useSelector(state => state.musicState.isPlaying)
     const currentTime = useSelector(state => state.musicState.currentTime)
+    const showYrlics = useSelector(state => state.musicState.showYrlics)
 
     useEffect(() => {
         const lyrics = playSongLyric.hasYrc ? playSongLyric.yrc : playSongLyric.lrc
@@ -20,20 +22,22 @@ const SideLyric = () => {
     }, [playSongId, playSongLyric])
 
 
-    return (lyricLines.length ? (
+    return (showYrlics && lyricLines.length ? (
             <div
                 style={{
                     "--amll-lyric-player-font-size": '1.4em',
                 }}
                 className={'relative max-w-full w-full h-[350px] rounded-xl overflow-hidden border mt-4'}
             >
-                <LyricPlayer className={"absolute inset-0 w-full h-full overflow-hidden"} style={{
-                    maxWidth: "100%",
-                    maxHeight: "100%",
-                    contain: "paint layout",
-                    mixBlendMode: "plus-lighter",
-                }} playing={isPlaying} currentTime={Math.round(currentTime)} enableSpring={false} enableScale={false}
-                             lyricLines={lyricLines} alignAnchor="center"/>
+                <NoSsr>
+                    <LyricPlayer className={"absolute inset-0 w-full h-full overflow-hidden"} style={{
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                        contain: "paint layout",
+                        mixBlendMode: "plus-lighter",
+                    }} playing={isPlaying} currentTime={Math.round(currentTime)} enableSpring={false} enableScale={false}
+                                 lyricLines={lyricLines} alignAnchor="center"/>
+                </NoSsr>
             </div>
         ) : null
     );
